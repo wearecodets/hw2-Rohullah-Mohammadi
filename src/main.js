2a4fc9970f0db391a184dc95c6878e23bcca1a2d
Rohullah <arohullah6@gmail.com>
2019-07-03 15:52:41 +0700

    fix submitBug

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 console.log('Hello, world!');
+const amount = document.querySelector('.form__total span');
+const subButton = document.querySelector('button[type="submit"]');
+
+const buttonAmount = document.querySelector('button span');
+// console.log(buttonAmount.textContent);
+// console.log(subButton.disabled);
+// console.log(amount);
+if (amount.textContent != '$0') {
+  subButton.disabled = false;
+  buttonAmount.textContent = amount.textContent;
+}
