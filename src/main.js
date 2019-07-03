da63fcf670bb90a281b95b7e4e133eaae821d2ae
Rohullah <arohullah6@gmail.com>
2019-07-03 16:06:37 +0700

    fix papal bug

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+const amount = document.querySelector('.form__total span');
+// console.log(amount);
+const paypalLink = document.querySelector('a.form__cc-heading');
+console.log(paypalLink);
+// paypalLink.href = 'www.goolge.com';
+function paypalweb() {
+  // paypalLink.removeAttribute('href');
+  paypalLink.setAttribute(
+    'href',
+    `https://paypal.me/nikosolihin/${amount.textContent}`
+  );
+}
+paypalLink.addEventListener('click', paypalweb);
