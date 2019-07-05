404c3a2c541995691adfad40a496096137ba57dd
Rohullah <49408851+Rohullah-Mohammadi@users.noreply.github.com>
2019-07-05 23:54:33 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 
-// console.log('Hello, world!');
 const amount = document.querySelector('.form__total span');
-// console.log(amount);
 const paypalLink = document.querySelector('a.form__cc-heading');
-console.log(paypalLink);
-// paypalLink.href = 'www.goolge.com';
 function paypalweb() {
-  // paypalLink.removeAttribute('href');
   paypalLink.setAttribute(
     'href',
     `https://paypal.me/nikosolihin/${amount.textContent}`
@@ -17,9 +12,6 @@ paypalLink.addEventListener('click', paypalweb);
 const inputs = document.querySelectorAll('input[name="plan"]');
 const annual = document.querySelector('input[value="Annually"]');
 const monthly = document.querySelector('input[value="Monthly"]');
-// console.log(inputs);
-// console.log(annual);
-// console.log(monthly);
 function changeCost() {
   if (annual.checked === true) {
     amount.textContent = '300';
@@ -30,17 +22,13 @@ function changeCost() {
 inputs.forEach(input => input.addEventListener('click', changeCost));
 
 console.log('Hello, world!');
-// const amount = document.querySelector('.form__total span');
 const subButton = document.querySelector('button[type="submit"]');
 
 const buttonAmount = document.querySelector('button span');
-// console.log(buttonAmount.textContent);
-// console.log(subButton.disabled);
-// console.log(amount);
-/*if (amount.textContent != '$0') {
+if (amount.textContent != '$0') {
   subButton.disabled = false;
   buttonAmount.textContent = amount.textContent;
-}*/
+}
 // console.log('Hello, world!');
 const accs = document.querySelectorAll('.accordion');
 // console.log(accs);
